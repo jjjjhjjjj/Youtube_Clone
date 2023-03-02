194b2a869dce9b6231afe190f7655c307adc4c04
Hj <jjjhjjjjj123@gmail.com>
2023-03-03 01:52:19 +0900

    Style: watch 페이지 CSS 작업

--- src/components/ViewVideo/ViewVideo.jsx
+++ src/components/ViewVideo/ViewVideo.jsx
@@ -1,18 +1,25 @@
+import styles from "./ViewVideo.module.css";
+
 export default function ViewVideo({ video }) {
   return (
-    <section>
-      <iframe
-        id="ytplayer"
-        type="text/html"
-        width="640"
-        height="360"
-        src={`https://www.youtube.com/embed/${video.id}`} //?autoplay=1
-        frameBorder="0"
-      ></iframe>
-      <div>
-        <p>{video.snippet.title}</p>
-        <strong>{video.snippet.channelTitle}</strong>
-        <p>{video.snippet.description}</p>
+    <section className={styles.container}>
+      <div className={styles.frame_box}>
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          src={`https://www.youtube.com/embed/${video.id}`} //?autoplay=1
+          height="100%"
+          width="100%"
+          frameBorder="0"
+          className={styles.iframe}
+        ></iframe>
+      </div>
+      <div className={styles.box}>
+        <p className={styles.title}>{video.snippet.title}</p>
+        <strong className={styles.channelTitle}>
+          [{video.snippet.channelTitle}]
+        </strong>
+        <p className={styles.desc}>{video.snippet.description}</p>
       </div>
     </section>
   );
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import Videos from "../components/Videos/Videos";
 import ViewVideo from "../components/ViewVideo/ViewVideo";
 import { getDataFromYoutubeAPIs, RELATED, VIDEO } from "../service/youtube";
@@ -8,16 +8,27 @@ export async function loader({ params }) {
 
   const video = await getDataFromYoutubeAPIs(VIDEO, { videoId });
   const { videos } = await getDataFromYoutubeAPIs(RELATED, { videoId });
+
   return { video, videos };
 }
 
 export default function Watch() {
   const { video, videos } = useLoaderData();
 
+  const navigate = useNavigate();
+
+  const handleWatchVideo = (videoId) => {
+    navigate(`/Watch/${videoId}`);
+  };
+
   return (
-    <>
-      <ViewVideo video={video} />
-      <Videos videos={videos} />
-    </>
+    <section className="video__container">
+      <div className="video__view">
+        <ViewVideo video={video} />
+      </div>
+      <div className="video__list-related">
+        <Videos videos={videos} onWatch={handleWatchVideo} />
+      </div>
+    </section>
   );
 }
